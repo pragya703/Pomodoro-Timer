88d810bd9b1a491c8cbb063ce7b8508f37c6c321
Pragya Anand <102343881+pragya703@users.noreply.github.com>
2023-04-17 17:16:21 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ["*"],
   theme: {
     boxShadow: {
-      'grayShadow': '-5px 10px 10px gray',
-      'clockOuterShadow': '6px 6px 10px -1px black 15%',
-      'clockInnerShadow': 'inset 4px 4px 6px -1px black 20%',
+      grayShadow: '-5px 10px 10px gray',
+      clockOuterShadow: '6px 6px 10px -1px black 15%',
+      clockInnerShadow: 'inset 4px 4px 6px -1px black 20%',
     },
     fontFamily: {
-      'Righteous': ['Righteous'],
-      'Audiowide': ['Audiowide'],
+      Righteous: ['Righteous'],
+      Audiowide: ['Audiowide'],
     },
     hover: {
       'default': 'rgb(255 255 255)',
